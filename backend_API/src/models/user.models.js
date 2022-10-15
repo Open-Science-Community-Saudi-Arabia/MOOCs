aff58f980ef08b96de2345fa91600e7b332f7aa4
Richie <molunorichie@gmail.com>
2022-10-15 10:52:14 +0100

    add: password reference, presave hook to save password - user.model.js

--- backend_API/src/models/user.models.js
+++ backend_API/src/models/user.models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Password = require('./password.models')
 const Schema = mongoose.Schema
 
 const options = { toObject: { virtuals: true } }
@@ -7,8 +8,37 @@ const user_schema = new Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true },
-    role: { type: String, required: true, enum: ['enduser', 'admin'] }
+    role: { type: String, required: true, enum: ['EndUser', 'Admin'] },
+    password: { type: String }
 }, options, { timestamp: true })
+
+// Virtualproperty to get user password from Password collection
+user_schema.virtual('password', {
+    ref: "Password",
+    localField: "_id",
+    foreignField: "user_id",
+    justOne: true
+})
+
+user_schema.pre('save', () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Save password reference for user in Password collection
+            await Password.create({
+                user_id: this._id,
+                password: this.password,
+                role: this.role
+            })
+    
+            // Set password field in User collection to null
+            this.password = null
+    
+            resolve(this)
+        } catch (error) {
+            reject(error)
+        }
+    })
+})
 
 const User = mongoose.model('User', user_schema)
 
