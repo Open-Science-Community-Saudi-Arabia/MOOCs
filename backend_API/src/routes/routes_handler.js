0d0298adfb503d4c76a5f650b5a954104f223a8f
Richie <molunorichie@gmail.com>
2022-10-15 12:12:41 +0100

    update: route path URL format - routes_handler.js

--- backend_API/src/routes/routes_handler.js
+++ backend_API/src/routes/routes_handler.js
@@ -1,6 +1,6 @@
 const authRoute = require('./auth.routes')
 
-// Route path format should start with /api/vi/
+// Route path format should start with /api/v1/
 module.exports = function (app) {
     app.use('/api/v1/auth', authRoute)
     // app.use('/api.v1/course', courseRoute)
