f65a7f2157c2052eecd3ac93e17a6bc09a919754
Meekunn <orofinadedamola@gmail.com>
2022-10-15 23:39:07 +0100

    Develop Header Section

--- Frontend/src/pages/landing-page/LandingPage.jsx
+++ Frontend/src/pages/landing-page/LandingPage.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 import "./landingpage.css";
 
 function LandingPage() {
-  return (
-    <div className="landing-page">
-      <h1>
-        <Link to="/login">Go to Login Page</Link>
-      </h1>
+	return (
+		<>
+			<Navbar />
+			<div className="landing-page">
+				<h1>
+					<Link to="/login">Go to Login Page</Link>
+				</h1>
 
-      <h1>
-        <Link to="/signup">Go to Sign Up Page</Link>
-      </h1>
-    </div>
-  );
+				<h1>
+					<Link to="/signup">Go to Sign Up Page</Link>
+				</h1>
+			</div>
+		</>
+	);
 }
 
 export default LandingPage;
