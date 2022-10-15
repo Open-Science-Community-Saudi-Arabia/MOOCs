91a8f594738687d07f51c3c8310aaabc2163a26b
Richie <molunorichie@gmail.com>
2022-10-15 12:13:21 +0100

    add: errorhandler middleware - app.js

--- backend_API/src/app.js
+++ backend_API/src/app.js
@@ -1,16 +1,20 @@
 
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require('cors');
+const errorHandler = require("./middlewares/error_handler");
 const app = express();
 
 
 // Middlewares
 app.use(morgan("dev"))
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
 // Route handler
 require('./routes/routes_handler')(app)
+
+// Error handler middleware
+app.use(errorHandler)
 
 module.exports = app;
