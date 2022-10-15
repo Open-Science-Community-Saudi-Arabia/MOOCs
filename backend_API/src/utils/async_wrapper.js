73962dcd272b40dfef68d23df239254af6f4465b
Richie <molunorichie@gmail.com>
2022-10-15 11:42:56 +0100

    add: asynchronous function wrappers - async_wrapper.js

---
+++ backend_API/src/utils/async_wrapper.js
@@ -0,0 +1,12 @@
+const asyncWrapper = (fn) => {
+    return async (req, res, next) => {
+        try {
+            return await fn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+
+module.exports = asyncWrapper
