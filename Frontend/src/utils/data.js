d2a07c76fa36407c8aab754355a74685522c3f66
mufidat3250 <wahabmufidat1919@gmail.com>
2022-10-16 21:14:59 +0000

    hero section and course carousel completed

---
+++ Frontend/src/utils/data.js
@@ -0,0 +1,77 @@
+import GlobalIcon from "../components/vectors/GlobalIcon";
+import Time from "../components/vectors/Time.jsx";
+import UsersIcon from "../components/vectors/UsersIcon.jsx";
+export const record = [
+  {
+    icon: Time,
+    value: "60,000+",
+    desc: "Hours content",
+  },
+  {
+    icon: GlobalIcon,
+    value: "1,00,000+",
+    desc: "Students",
+  },
+  {
+    icon: UsersIcon,
+    value: "110+",
+    desc: "Countries",
+  },
+];
+
+export const courses = [
+  {
+    img: "../../public/images/course3.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.5'
+  },
+  {
+    img: "../../public/images/course1.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.0'
+  },
+  {
+    img: "../../public/images/course2.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.7'
+  },
+  {
+    img: "../../public/images/course1.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.5'
+  },
+  {
+    img: "../../public/images/course3.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.0'
+  },
+  {
+    img: "../../public/images/course2.png",
+    title: "Creative Writing",
+    desc: `Creative Writing : Creative writing is
+        any writing that goes outside 
+       the bounds of normal professional `,
+    lessons: "12x Lessons",
+    ratings:'4.7'
+  },
+];
