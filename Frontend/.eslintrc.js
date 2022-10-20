1a7e1588ec8402c13f6bd5b81722f208c710d3ac
Sau <julienyange@gmail.com>
2022-10-20 21:43:18 +0000

    feat: set up eslint

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -4,12 +4,21 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ["plugin:react/recommended", "airbnb"],
+    extends: ["plugin:react/recommended", "airbnb", "prettier"],
     overrides: [],
     parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
     },
-    plugins: ["react"],
-    rules: {},
+    plugins: ["react", "prettier"],
+    rules: {
+        "prettier/prettier": ["error"],
+        "no-unused-vars": "warn",
+        "react/no-unescaped-entities": 0,
+        "jsx-a11y/anchor-is-valid": 0,
+        "react/button-has-type": 0,
+        "react/self-closing-comp": "warn",
+        "import/newline-after-import": "warn",
+        "import/order": "warn"
+    }
 }
