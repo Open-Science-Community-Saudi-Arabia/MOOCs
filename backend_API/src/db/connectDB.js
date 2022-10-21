835b149c56b0c9f7e6f992d70db97cd39a1bf1ff
Richie <molunorichie@gmail.com>
2022-10-21 16:30:45 +0100

    add: condition for morgan('dev') middlware - app.js

--- backend_API/src/db/connectDB.js
+++ backend_API/src/db/connectDB.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 async function connectDatabase(url) {
     return new Promise((resolve, reject) => {
         mongoose.connect(url)
-        .then((response) => {
-            console.log(`Connection to ${mongoose.connection.name} database Successful!`);
-            resolve('Successful')
-        }, (error) => {
-            console.log(error)
-            reject(error)
-        });
+            .then((response) => {
+                console.log(`Connection to ${mongoose.connection.name} database Successful!`);
+                resolve('Successful')
+            }, (error) => {
+                console.log(error)
+                reject(error)
+            });
 
     })
 }
