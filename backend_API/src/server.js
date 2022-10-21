342f79877798df1a5692e9d0bbb33bf58f02a7b1
Richie <molunorichie@gmail.com>
2022-10-21 16:30:45 +0100

    add: condition for morgan('dev') middlware - app.js

--- backend_API/src/server.js
+++ backend_API/src/server.js
@@ -8,7 +8,7 @@ const connectDatabase = require('./db/connectDB')
 async function start() {
   try {
     await connectDatabase(MONGO_URI)
-
+    
     app.listen(PORT, function () {
       console.log(`Server is running on port ${PORT}....`)
     })
