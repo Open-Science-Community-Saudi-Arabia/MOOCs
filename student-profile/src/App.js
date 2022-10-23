ec4c3f996f885eab2448850aa0ece68813ea3c03
ladyami <99187115+ladyami@users.noreply.github.com>
2022-10-23 23:26:58 +0100

    created a student dashboard with reactjs

---
+++ student-profile/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./Component/SideBar Section/Sidebar";
+import Body from "./Component/Body Section/Body";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
