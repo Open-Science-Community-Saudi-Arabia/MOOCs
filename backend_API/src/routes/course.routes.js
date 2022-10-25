9b9fc136d62b5eef4ea984e5d17bee55f37ffc6d
Ebuka <ebukaobiora10@gmail.com>
2022-10-25 20:08:32 +0100

    Admin create course

--- backend_API/src/routes/course.routes.js
+++ backend_API/src/routes/course.routes.js
@@ -1,9 +1,11 @@
 
 const router = require("express")()
 
-const {getCourses, deleteCourse} = require("../controllers/courses.controller")
+const {createCourse, getCourses, deleteCourse} = require("../controllers/courses.controller")
 const permission = require("../middlewares/permission_handler")
 const {basicAuth} = require("../middlewares/auth")
+
+router.post("/create-course/:courseId",basicAuth, permission("Admin"), createCourse)
 
 router.get("/get-courses", basicAuth, permission("Admin EndUser"), getCourses)
 
