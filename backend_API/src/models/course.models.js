0902eff0f51170bdf15dc2966516212daae262bd
Richie <molunorichie@gmail.com>
2022-10-26 23:13:56 +0100

    add: exercise shcema model - course.models.js

--- backend_API/src/models/course.models.js
+++ backend_API/src/models/course.models.js
@@ -1,6 +1,30 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
+
+const exerciseSchema = new Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    // Assuming exercises are in quiz format
+    alternatives: [
+        {
+            text: {
+                type: String,
+                required: true
+            },
+            isCorrect: {
+                type: Boolean,
+                required: true,
+                default: false
+            }
+        }
+    ],
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+}, {
+    timestamps: true,
+})
+
 const videoSchema = new mongoose.Schema({
     adminId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -39,8 +63,10 @@ const courseSchema = new mongoose.Schema({
         required: true
     },
     videos: [{ type: mongoose.Types.ObjectId, ref: "Video" }],
+    exercises: [{type: mongoose.Types.ObjectId, ref: "Exercise"}],
 }, { timestamps: true })
 
+const Exercise = mongoose.model("Exercise", exerciseSchema)
 const Video = mongoose.model("Video", videoSchema)
 const Course = mongoose.model("Course", courseSchema)
 
