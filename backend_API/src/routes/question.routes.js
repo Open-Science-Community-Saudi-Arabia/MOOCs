ab91a1fe5db6f44039da3c09dcbf7d7fa6dfd8e3
Richie <molunorichie@gmail.com>
2022-10-29 23:20:19 +0100

    add: routes for CRUD operations for questions - question.routes.js

---
+++ backend_API/src/routes/question.routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const permit = require("../middlewares/permission_handler");
+const { basicAuth } = require("../middlewares/auth");
+
+const { createQuestion, getQuestions, deleteQuestion, updateQuestion, scoreAnswers } = require("../controllers/question.controllers")
+
+router.all('/', basicAuth, permit('Admin'))
+
+router
+    .post("/new", createQuestion)
+    .get("/", permit('EndUser'), getQuestions)
+    .patch("/update/:id", updateQuestion)
+    .delete("/delete/:id", deleteQuestion)
+    .post("/grade", permit('EndUser'), scoreAnswers)
+
+
