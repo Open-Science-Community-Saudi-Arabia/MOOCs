975e1edcaf6d9c11dc0a4f0d96d8f84f5fa2977c
Richie <molunorichie@gmail.com>
2022-10-31 20:49:51 +0100

    update permission handler import - course.routes.js

--- backend_API/src/routes/course.routes.js
+++ backend_API/src/routes/course.routes.js
@@ -2,21 +2,21 @@
 const router = require("express").Router();
 
 const { createCourse, getCourses, deleteCourse, updateCourse, uploadVideo, getVideo, updateVideo } = require("../controllers/course.controllers")
-const permission = require("../middlewares/permission_handler")
+const permit = require("../middlewares/permission_handler")
 const { basicAuth } = require("../middlewares/auth")
 
-router.all('/', basicAuth)
+// router.all('/', basicAuth)
 
 router
-    .post("/new", permission("Admin"), createCourse)
-    .get("/", permission("Admin EndUser"), getCourses)
-    .patch("/update/:id", permission("Admin"), updateCourse)
-    .delete("/delete/:courseId", permission("Admin"), deleteCourse)
+    .post("/new", permit("Admin"), createCourse)
+    .get("/", permit("Admin EndUser"), getCourses)
+    .patch("/update/:id", permit("Admin"), updateCourse)
+    .delete("/delete/:courseId", permit("Admin"), deleteCourse)
 
 router
-    .post("/video/upload", permission("Admin"), uploadVideo)
-    .get("/course/video", permission("Admin EndUser"), getVideo)
-    .patch("/video/update/:id", permission("Admin"), updateVideo)
+    .post("/video/upload", permit("Admin"), uploadVideo)
+    .get("/course/video", permit("Admin EndUser"), getVideo)
+    .patch("/video/update/:id", permit("Admin"), updateVideo)
 
 
 
