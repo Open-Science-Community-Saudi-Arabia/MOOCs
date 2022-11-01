4ff5d210dca1b5a731efd4b721f3ffc40d86fe99
Richie <molunorichie@gmail.com>
2022-11-01 23:34:38 +0100

    add: /enroll /cancelenrollment routes - course.routes.js

--- backend_API/src/routes/course.routes.js
+++ backend_API/src/routes/course.routes.js
@@ -1,7 +1,7 @@
 
 const router = require("express").Router();
 
-const { createCourse, getCourses, deleteCourse, updateCourse, uploadVideo, getVideo, updateVideo } = require("../controllers/course.controllers")
+const { createCourse, getCourses, deleteCourse, updateCourse, uploadVideo, getVideo, updateVideo, enrollCourse, cancelEnrollment } = require("../controllers/course.controllers")
 const permit = require("../middlewares/permission_handler")
 const { basicAuth } = require("../middlewares/auth")
 
@@ -12,6 +12,8 @@ router
     .get("/", permit("Admin EndUser"), getCourses)
     .patch("/update/:id", permit("Admin"), updateCourse)
     .delete("/delete/:courseId", permit("Admin"), deleteCourse)
+    .post("/enroll", permit("Admin EndUser"), enrollCourse)
+    .post("/cancelenrollment", permit("Admin EndUser"), cancelEnrollment)
 
 router
     .post("/video/upload", permit("Admin"), uploadVideo)
