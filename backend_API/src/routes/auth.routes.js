5ad56d90497953e80872b7861786390d72ffaa30
Richie <molunorichie@gmail.com>
2022-11-30 17:37:23 +0100

    update reset password route - auth.routes.js

--- backend_API/src/routes/auth.routes.js
+++ backend_API/src/routes/auth.routes.js
@@ -6,7 +6,7 @@ router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 
 router.post('/forgotpassword', authController.forgetPassword)
-router.patch('/resetpassword/:token', authController.resetPassword)
+router.patch('/resetpassword/', authController.resetPassword)
 router.post('/googlesignin', authController.googleSignin)
 router.get('/verifyemail/:token', authController.verifyEmail)
 
