80f61f6d3f926abcbca6757a9973f651da132672
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-11-30 14:21:44 +0000

    added update

--- Frontend/src/utils/api/google/index.jsx
+++ Frontend/src/utils/api/google/index.jsx
@@ -1,14 +1,12 @@
 
-import  {  useEffect } from "react"
+import { useEffect } from "react"
 import axios from 'axios'
 import { setToken } from '../../index'
 
 const baseURL = import.meta.env.VITE_API_BASEURL
 const googleID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
-const {google} = window;
-
-function GoogleLogin({ setLoadingBoard}) {
+function GoogleLogin({ setLoadingBoard }) {
   async function handleGoogle(response) {
     try {
       setLoadingBoard(true)
@@ -23,7 +21,7 @@ function GoogleLogin({ setLoadingBoard}) {
         }).then((responsedata) => {
           const tokenAccess = responsedata.data.token
           setToken(tokenAccess)
-          return  window.location.assign('/dashboard')
+          return window.location.assign('/dashboard')
         })
     } catch (error) {
       setLoadingBoard(false)
@@ -33,20 +31,18 @@ function GoogleLogin({ setLoadingBoard}) {
     return null
   }
   useEffect(() => {
-
-    if (window.google) {
-      google.accounts.id.initialize({
-        client_id: googleID ,
+     window.google.accounts.id.initialize({
+        client_id: googleID,
         callback: handleGoogle,
       });
 
-      google.accounts.id.renderButton(document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" } );
-    }
-  },[]);
+      window.google.accounts.id.renderButton(document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" });
+  
+  }, []);
 
-  
-  
+
+
 }
 
 export default GoogleLogin
