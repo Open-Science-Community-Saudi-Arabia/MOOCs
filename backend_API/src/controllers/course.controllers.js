0bd00ad053fd19c4d123c43ea688d31ce2c444a0
Richie <molunorichie@gmail.com>
2023-01-02 20:38:45 +0100

    add: missing imports in course.controllers.js

--- backend_API/src/controllers/course.controllers.js
+++ backend_API/src/controllers/course.controllers.js
@@ -1,174 +1,163 @@
-const { Video, Course, Exercise, Question } = require("../models/course.models")
-const { v2 } = require("cloudinary")
+const {
+  Video,
+  Course,
+  Exercise,
+  Question,
+} = require("../models/course.models");
+const { v2 } = require("cloudinary");
 const asyncWrapper = require("../utils/async_wrapper");
 const { BadRequestError } = require("../utils/custom_errors");
+const User = require("../models/user.models");
 
 /* COURSES */
 
 // Create a new course
-exports.createCourse = asyncWrapper(
-    async (req, res, next) => {
-        const newCourse = new Course(req.body);
-        const savedCourse = await newCourse.save();
-        res.status(200).json(savedCourse);
-    }
-)
+exports.createCourse = async (req, res, next) => {
+  const newCourse = new Course(req.body);
+  const savedCourse = await newCourse.save();
+  res.status(200).json(savedCourse);
+};
 
 // Get data for all course - req.body._id = null
 // Get all data for a particular course - req.body._id = the id of the course you want to get data for
 // Get all courses - req.body = null
-exports.getCourses = asyncWrapper(
-    async (req, res, next) => {
-        if (req.body) {
-            const courses = await Course.find(req.body)
-            res.status(200).json(courses);
-        }
-        const courses = await Course.find().sort({ _id: -1 })
+exports.getCourses = async (req, res, next) => {
+  if (req.body) {
+    const courses = await Course.find(req.body);
+    res.status(200).json(courses);
+  }
+  const courses = await Course.find().sort({ _id: -1 });
 
-        return res.status(200).send({ courses: courses })
-    }
-)
+  return res.status(200).send({ courses: courses });
+};
 
 // Update data for a particular course
-exports.updateCourse = asyncWrapper(
-    async (req, res, next) => {
-        const course = await Course.findById(req.params.id);
+exports.updateCourse = async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
 
-        if (course) {
-            await course.updateOne({ $set: req.body });
+  if (course) {
+    await course.updateOne({ $set: req.body });
 
-            return res.status(200).json({ message: "Course Updated", course: course });
-        }
+    return res.status(200).json({ message: "Course Updated", course: course });
+  }
 
-        next(new BadRequestError("Course not found"));
-    }
-)
+  next(new BadRequestError("Course not found"));
+};
 
 // Delete a particular course
-exports.deleteCourse = asyncWrapper(
-    async (req, res, next) => {
-        const courseId = req.params.courseId
-        await Course.findByIdAndDelete(courseId)
+exports.deleteCourse = async (req, res, next) => {
+  const courseId = req.params.courseId;
+  await Course.findByIdAndDelete(courseId);
 
-        res.status(200).send({ message: "course has been deleted successfully" })
-    }
-)
+  res.status(200).send({ message: "course has been deleted successfully" });
+};
 
 // Enroll a user in a course
-exports.enrollCourse = asyncWrapper(
-    async (req, res, next) => {
-        const course = await Course.findById(req.body.course_id)
-        const user = await User.findById(req.body.user_id)
+exports.enrollCourse = async (req, res, next) => {
+  const course = await Course.findById(req.body.course_id);
+  const user = await User.findById(req.body.user_id);
 
-        course.enrolled_users.push(user)
-        await course.save()
+  course.enrolled_users.push(user);
+  await course.save();
 
-        user.enrolled_courses.push(course)
-        await user.save()
+  user.enrolled_courses.push(course);
+  await user.save();
 
-        res.status(200).send({ message: "user has been enrolled in course successfully" })
-    }
-)
+  res
+    .status(200)
+    .send({ message: "user has been enrolled in course successfully" });
+};
 
 // Cancel enrollment of a user in a course
-exports.cancelEnrollment = asyncWrapper(
-    async (req, res, next) => {
-        const course = await Course.findById(req.body.course_id)
-        const user = await User.findById(req.body.user_id)
+exports.cancelEnrollment = async (req, res, next) => {
+  const course = await Course.findById(req.body.course_id);
+  const user = await User.findById(req.body.user_id);
 
-        course.enrolled_users.pull(user)
-        await course.save()
+  course.enrolled_users.pull(user);
+  await course.save();
 
-        user.enrolled_courses.pull(course)
-        await user.save()
+  user.enrolled_courses.pull(course);
+  await user.save();
 
-        res.status(200).send({ message: "user has been unenrolled from course successfully" })
-    }
-)
+  res
+    .status(200)
+    .send({ message: "user has been unenrolled from course successfully" });
+};
 
 // Get all enrolled courses for a particular user
-exports.getEnrolledCourses = asyncWrapper(
-    async (req, res, next) => {
-        const user = await User.findById(req.body.user_id)
-        const enrolledCourses = await Course.find({ _id: { $in: user.enrolled_courses } })
+exports.getEnrolledCourses = async (req, res, next) => {
+  const user = await User.findById(req.body.user_id);
+  const enrolledCourses = await Course.find({
+    _id: { $in: user.enrolled_courses },
+  });
 
-        res.status(200).send({ enrolledCourses: enrolledCourses })
-    }
-)
+  res.status(200).send({ enrolledCourses: enrolledCourses });
+};
 
 // Get all enrolled users for a particular course
-exports.getEnrolledUsers = asyncWrapper(
-    async (req, res, next) => {
-        const course = await Course.findById(req.body.course_id)
-        const enrolledUsers = await User.find({ _id: { $in: course.enrolled_users } })
+exports.getEnrolledUsers = async (req, res, next) => {
+  const course = await Course.findById(req.body.course_id);
+  const enrolledUsers = await User.find({
+    _id: { $in: course.enrolled_users },
+  });
 
-        res.status(200).send({ enrolledUsers: enrolledUsers })
-    }
-)
-
+  res.status(200).send({ enrolledUsers: enrolledUsers });
+};
 
 /* VIDEOS */
 
 // Upload a video
-exports.uploadVideo = asyncWrapper(
-    async (req, res, next) => {
-        const { video } = req.files
-        const { course_id } = req.body
+exports.uploadVideo = asyncWrapper(async (req, res, next) => {
+  const { video } = req.files;
+  const { course_id } = req.body;
 
-        const result = await v2.uploader.upload(video.tempFilePath, {
-            resource_type: "video",
-            folder: "videos"
-        })
+  const result = await v2.uploader.upload(video.tempFilePath, {
+    resource_type: "video",
+    folder: "videos",
+  });
 
-        const newVideo = new Video({
-            video_url: result.secure_url,
-            video_id: result.public_id,
-            ...req.body
-            //     course: course_id,
-            //     description: req.body.description,
-            //     title: req.body.title,
-            //     author: req.body.author
-        })
+  const newVideo = new Video({
+    video_url: result.secure_url,
+    video_id: result.public_id,
+    ...req.body,
+    //     course: course_id,
+    //     description: req.body.description,
+    //     title: req.body.title,
+    //     author: req.body.author
+  });
 
-        const savedVideo = await newVideo.save()
-        res.status(200).json(savedVideo);
-    }
-)
+  const savedVideo = await newVideo.save();
+  res.status(200).json(savedVideo);
+});
 
 // Get data for particular video - req.body._id = video_id
 // Get data for all videos - req.body._id = null
 // Get videos for a particular course - req.body.course_id = course_id
 // Get all videos - req.body = null
-exports.getVideo = asyncWrapper(
-    async (req, res, next) => {
-
-        const videos = await Video.find(req.body)
-        res.status(200).json(videos);
-    }
-)
+exports.getVideo = asyncWrapper(async (req, res, next) => {
+  const videos = await Video.find(req.body);
+  res.status(200).json(videos);
+});
 
 // Update data for a particular video
-exports.updateVideo = asyncWrapper(
-    async (req, res, next) => {
-        const video = await Video.findById(req.params.id);
+exports.updateVideo = asyncWrapper(async (req, res, next) => {
+  const video = await Video.findById(req.params.id);
 
-        if (video) {
-            await video.updateOne({ $set: req.body });
+  if (video) {
+    await video.updateOne({ $set: req.body });
 
-            return res.status(200).json({ message: "Video Updated", video: video });
-        }
+    return res.status(200).json({ message: "Video Updated", video: video });
+  }
 
-        next(new BadRequestError("Video not found"));
-    }
-)
+  next(new BadRequestError("Video not found"));
+});
 
 // Delete a particular video
-exports.deleteVideo = asyncWrapper(
-    async (req, res, next) => {
+exports.deleteVideo = asyncWrapper(async (req, res, next) => {
+  const videoId = req.params.videoId;
+  await Video.findByIdAndDelete(videoId);
 
-        const videoId = req.params.videoId
-        await Video.findByIdAndDelete(videoId)
-
-        return res.status(200).send({ message: "video has been deleted successfully" })
-    }
-)
+  return res
+    .status(200)
+    .send({ message: "video has been deleted successfully" });
+});
