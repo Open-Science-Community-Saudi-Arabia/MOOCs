acb9d19bcec4582887e449bcf2be8d06997664c1
Richie <molunorichie@gmail.com>
2023-01-08 02:53:47 +0100

    switched to google SDK for googlesignin - auth.route.js

--- backend_API/src/routes/auth.routes.js
+++ backend_API/src/routes/auth.routes.js
@@ -3,13 +3,12 @@ const { basicAuth } = require('../middlewares/auth');
 const authController = require('./../controllers/auth.controllers');
 const router = express.Router();
 const passport = require('passport');
-const { googleStrategy, githubStrategy } = require('../utils/passport');
+const { githubStrategy } = require('../utils/passport');
 
 /**
  * @todo - Improve modularity for passport
  */
-passport.use(googleStrategy);
-passport.use(githubStrategy)
+passport.use(githubStrategy);
 passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
@@ -24,20 +23,16 @@ router.post('/login', authController.login);
 
 router.post('/forgotpassword', authController.forgetPassword);
 router.patch('/resetpassword/', authController.resetPassword);
+router.get('/google', authController.googleSignin);
 router.get(
-    '/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
+    '/github',
+    passport.authenticate('github', { scope: ['user:email'] })
 );
 router.get(
-    '/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/login',
-        failureMessage: true,
-    }),
+    '/github/callback',
+    passport.authenticate('github'),
     authController.passportOauthCallback
 );
-router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
-router.get('/github/callback', passport.authenticate('github'), authController.passportOauthCallback);
 router.post('/google/callback', authController.googleSignin);
 router.get('/verifyemail/:token', authController.verifyEmail);
 router.get('/user', authController.getLoggedInUser);
