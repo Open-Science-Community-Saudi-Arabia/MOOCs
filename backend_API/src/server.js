31c07256b2f2a59c8ba9817e4e8f6d6a3d91d336
Richie <molunorichie@gmail.com>
2023-01-15 03:20:40 +0100

    add: getMongoURI for connnection string in each NODE ENV - server.js

--- backend_API/src/server.js
+++ backend_API/src/server.js
@@ -4,16 +4,19 @@ if (process.env.NODE_ENV) {
     require('dotenv').config({ path: `${__dirname}/.env` });
 }
 
-console.log(__dirname);
-const { MONGO_URI, PORT } = require('./utils/config');
-
+const config = require('./utils/config');
 const app = require('./app');
 
 const connectDatabase = require('./db/connectDB');
 
+function getMongoURI() {
+    return config['MONGO_URI' + (process.env.NODE_ENV ? `_${process.env.NODE_ENV.toUpperCase()}` : '')];
+}
+
+const PORT = config.PORT;
 async function start() {
     try {
-        await connectDatabase(MONGO_URI);
+        await connectDatabase(getMongoURI());
 
         app.listen(PORT, function () {
             console.log(`Server is running on port ${PORT}....`);
