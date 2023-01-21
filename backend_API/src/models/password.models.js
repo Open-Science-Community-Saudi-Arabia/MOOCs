b3f936dc892a8a9383d070fbe40b61cd61cd16da
Richie <molunorichie@gmail.com>
2023-01-21 13:06:58 +0100

    add: schema methods for updatePassword and comparePassword - password.model.js

--- backend_API/src/models/password.models.js
+++ backend_API/src/models/password.models.js
@@ -20,6 +20,19 @@ passwordSchema.pre('save', async function (next) {
     next();
 })
 
+passwordSchema.methods.updatePassword = async function (newPassword) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(newPassword, salt);
+
+    await this.save();
+}
+
+passwordSchema.methods.comparePassword = async function (password) {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+}
+
+
 const Password = mongoose.model('Password', passwordSchema);
 
 module.exports = Password;
