0c88764adc566b6d6e90cc08f81debee45bc4ada
Richie <molunorichie@gmail.com>
2023-01-24 04:20:51 +0100

    add: routes for addAdmin and superAdmin activation - auth.routes.js

--- backend_API/src/routes/auth.routes.js
+++ backend_API/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { basicAuth } = require('../middlewares/auth');
+const rbac = require('../middlewares/permission_handler');
 const authController = require('./../controllers/auth.controllers');
 const router = express.Router();
 const passport = require('passport');
@@ -18,23 +19,26 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-
-router.post('/forgotpassword', authController.forgetPassword);
-router.patch('/resetpassword/', basicAuth('password_reset'), authController.resetPassword);
-router.get('/google', authController.googleSignin);
-router.get(
-    '/github',
-    passport.authenticate('github', { scope: ['user:email'] })
-);
-router.get(
-    '/github/callback',
-    passport.authenticate('github'),
-    authController.passportOauthCallback
-);
-router.post('/google/callback', authController.googleSignin);
-router.get('/verifyemail/:token', authController.verifyEmail);
-router.get('/user', authController.getLoggedInUser);
+router
+    .post('/signup', authController.signup)
+    .post('/addadmin', rbac('SuperAdmin'), authController.addAdmin)
+    .get('/superadmin/reqactivate', authController.requestSuperAdminAccountActivation)
+    .post('/superadmin/activate', authController.activateSuperAdminAccount)
+    .post('/login', authController.login)
+    .post('/forgotpassword', authController.forgetPassword)
+    .patch('/resetpassword/', basicAuth('password_reset'), authController.resetPassword)
+    .get('/google', authController.googleSignin)
+    .get(
+        '/github',
+        passport.authenticate('github', { scope: ['user:email'] })
+    )
+    .get(
+        '/github/callback',
+        passport.authenticate('github'),
+        authController.passportOauthCallback
+    )
+    .post('/google/callback', authController.googleSignin)
+    .get('/verifyemail/:token', authController.verifyEmail)
+    .get('/user', authController.getLoggedInUser)
 
 module.exports = router;
