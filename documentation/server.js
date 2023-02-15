1db153891dd5aa7c8bcd85fd9500d2201ac3b905
Richie <molunorichie@gmail.com>
2023-02-15 17:26:37 +0100

    remove port variable alternative

--- documentation/server.js
+++ documentation/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config({ path: `${__dirname}/.env`})
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT
 
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/docs'));
+app.use(express.static('./docs'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}....`);
