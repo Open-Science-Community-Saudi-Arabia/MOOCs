9917fa9017cca377ad762ef30bc40221f0bb0c9b
Richie <molunorichie@gmail.com>
2023-02-20 22:13:58 +0100

    update jsdoc config

--- documentation/server.js
+++ documentation/server.js
@@ -7,7 +7,7 @@ const app = express();
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
-app.use(express.static('./'));
+app.use(express.static('./docs/'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}....`);
