ca739867fcc4b6699be3a7ccc13b708e3e8d9f8e
Richie <molunorichie@gmail.com>
2023-02-21 15:47:33 +0100

    add: log config

--- API/src/utils/passport.js
+++ API/src/utils/passport.js
@@ -5,7 +5,7 @@ const config = require('./config');
 const User = require('../models/user.models');
 const UUID = require('uuid').v4;
 
-console.log(config)
+console.log(process.env)
 
 const createUser = async (data) => {
     try {
