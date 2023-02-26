5770db650600b112ba3a78146415e16a5302b01f
Richie <Molunorichie@gmail.com>
2023-02-26 12:24:11 +0000

    Revert "deleted backend_API folder"
    This reverts commit 87ee7b1ff22ee3e3662daaf03400f27694052a89.

---
+++ backend_API/src/models/token.models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+const { JWT_REFRESH_EXP } = require('../utils/config')
+
+const blacklistedTokenSchema = new schema(
+    {
+        token: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+    },
+    { timestamps: true, expires: JWT_REFRESH_EXP }
+)
+
+const authCodeSchema = new schema(
+    {
+        user: { type: schema.Types.ObjectId, ref: 'User', required: true },
+        verification_code: { type: String },
+        password_reset_code: { type: String },
+        activation_code: { type: String },
+        deactivation_code: { type: String },
+        createdAt: { type: Date, default: Date.now },
+        // expiresIn: { type: Date, default: Date.now + JWT_REFRESH_EXP },
+    },
+    { timestamps: true }
+)
+
+const AuthCode = mongoose.model('AuthCode', authCodeSchema)
+
+const BlacklistedToken = mongoose.model(
+    'BlacklistedToken',
+    blacklistedTokenSchema
+)
+
+module.exports = { BlacklistedToken, AuthCode }
---
+++ backend_API/src/utils/auth_codes.js
@@ -0,0 +1,27 @@
+const User = require('../models/user.models');
+const TestToken = require('../models/token.models');
+const AuthCode = require('../models/authcode.models');
+const { sendEmail } = require('../utils/email');
+
+const { CustomAPIError, BadRequestError } = require('./errors');
+
+// Function to generate random code for password reset
+const getAuthCodes = async (user_id, code_type = 'password_reset') => {
+
+    // 1. Check if user exists
+    const user = await User.findById(user_id);
+
+    if (!user) { throw new NotFoundError('User not found'); }
+
+    // 2. Generate random code
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+    // 3. Check if code exists
+    const authCodes = await AuthCode.findOneAndUpdate({ user: user_id }, { [code_type]: code }, { new: true, upsert: true });
+
+    return authCodes;
+};
+
+module.exports = {
+    getAuthCodes,
+}
