682539871b8d2581fa60d6a68223db45a0e67ceb
Richie <molunorichie@gmail.com>
2023-03-04 23:52:24 +0100

    add: question routes - question.routes.js

--- API/src/routes/question.routes.js
+++ API/src/routes/question.routes.js
@@ -2,15 +2,19 @@ const router = require("express").Router();
 const permit = require("../middlewares/permission_handler");
 const { basicAuth } = require("../middlewares/auth");
 
-const { createQuestion, getQuestions, deleteQuestion, updateQuestion, scoreAnswers } = require("../controllers/question.controllers")
+const {
+    createQuestion, getQuestions,
+    deleteQuestion, updateQuestion,
+    getQuestionData
+} = require("../controllers/question.controllers")
 
-router.all('/', basicAuth, permit('Admin'))
+router.use(basicAuth())
 
 router
-    .post("/new", createQuestion)
     .get("/", permit('EndUser'), getQuestions)
+    .get("/:id", permit('EndUser'), getQuestionData)
+    .post("/new", createQuestion)
     .patch("/update/:id", updateQuestion)
     .delete("/delete/:id", deleteQuestion)
-    .post("/grade", permit('EndUser'), scoreAnswers)
 
 module.exports = router
