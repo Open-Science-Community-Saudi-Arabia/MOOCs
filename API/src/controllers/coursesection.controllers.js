1a624037ec2114565492d72777cdbf817fbd9a1f
Richie <molunorichie@gmail.com>
2023-03-10 16:35:13 +0100

    add: dummy functions for coursesection

---
+++ API/src/controllers/coursesection.controllers.js
@@ -0,0 +1,23 @@
+exports.createCourseSection = async (req, res, next) => {
+
+}
+
+exports.getCourseSectionData = async (req, res, next) => {
+
+}
+
+exports.hideCourseSection = async (req, res, next) => {
+
+}
+
+exports.showCourseSection = async (req, res, next) => {
+
+}
+
+exports.deleteCourseSection = async (req, res, next) => {
+    
+}
+
+exports.addVideoToCourseSection = async (req, res, next) => {
+    
+}
