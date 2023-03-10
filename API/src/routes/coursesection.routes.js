453cecb961779aed6c033c46eea3d240d7dc977c
Richie <molunorichie@gmail.com>
2023-03-10 16:55:24 +0100

    add: coursesection routes

---
+++ API/src/routes/coursesection.routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    addCourseSection, getCourseSectionData, updateCourseSection, deleteCourseSection, addVideoToCourseSection, addExerciseToCourseSection,
+} = require('../controllers/coursesection.controllers')
+
+const { basicAuth } = require('../middlewares/auth')
+const permit = require('../middlewares/permission_handler')
+
+router.use(basicAuth(), permit('Admin SuperAdmin EndUser'))
+
+router
+    .post('/new', addCourseSection)
+    .get('/:id', getCourseSectionData)
+    .patch('/update/:id', updateCourseSection)
+    .delete('/delete/:id', deleteCourseSection)
+    .post('/video/link', addVideoToCourseSection)
+    .post('/exercise/link', addExerciseToCourseSection)
+
+module.exports = router
