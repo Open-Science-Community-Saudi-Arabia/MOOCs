19d821a945df7e674b147dc2545f43f474a618a0
Richie <molunorichie@gmail.com>
2023-03-12 11:28:56 +0100

    update route import for textmaterial - routes_handler.js

--- API/src/routes/routes_handler.js
+++ API/src/routes/routes_handler.js
@@ -3,6 +3,7 @@ const courseRoute = require("./course.routes")
 const courseSectionRoute = require('./coursesection.routes')
 const exerciseRoute = require("./exercise.routes")
 const questionRoute = require("./question.routes")
+const textmaterialRoute = require("./textmaterial.routes")
 
 // Route path format should start with /api/v1/
 module.exports = function (app) {
@@ -11,5 +12,6 @@ module.exports = function (app) {
     app.use('/api/v1/coursesection', courseSectionRoute)
     app.use('/api/v1/exercise', exerciseRoute)
     app.use('/api/v1/question', questionRoute)
+    app.use('/api/v1/textmaterial', textmaterialRoute)
     // app.use('/api/v1/course', courseRoute)
 }
