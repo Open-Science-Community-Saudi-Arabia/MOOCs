aca03ff239c0bafe8fb94a4608d4e62240851ca1
Richie <molunorichie@gmail.com>
2023-03-13 00:18:18 +0100

    complete issue certificate

--- API/src/controllers/certificate.controllers.js
+++ API/src/controllers/certificate.controllers.js
@@ -24,11 +24,12 @@ exports.getAllUsersCertificates = async (req, res, next) => {
  * @param {MongooseDocument} student_course_report 
  * @returns {MongooseDocument} certificate
  */
-async function createCertificate (student_course_report) {
+async function createCertificate(student_course_report) {
     const certificate = new Certificate({
         user: student_course_report.user,
         course: student_course_report.course,
         course_report: student_course_report._id,
+        serial_number: Date.now(),
     });
 
     // Create certificate image
@@ -43,21 +44,34 @@ async function createCertificate (student_course_report) {
     context.drawImage(image, 0, 0, image.width, image.height)
 
     // Draw text on canvas
-    context.font = "bold 30px Arial"
+    context.font = "italic 80px Arial"
     context.fillStyle = "black"
     context.textAlign = "center"
 
     // Add user's fullname
-    const users_fullmame = `${student_course_report.user.firstname} ${student_course_report.user.lastname}`
-    context.fillText(users_fullmame, 500, 300)
+    let users_firstname = student_course_report.user.firstname,
+        users_lastname = student_course_report.user.lastname
+
+    // Capitalize first letter of firstname and lastname
+    users_firstname = users_firstname.charAt(0).toUpperCase() + users_firstname.slice(1)
+    users_lastname = users_lastname.charAt(0).toUpperCase() + users_lastname.slice(1)
+
+    const users_fullmame = `${users_firstname} ${users_lastname}`
+    context.fillText(users_fullmame, 1000, 900)
 
     // Add course title
     const course_title = student_course_report.course.title
-    context.fillText(course_title, 500, 350)
+    context.fillText(course_title, 1000, 100)
+    
+    // Add certificate id
+    const certificate_id = certificate.serial_number.toString()
+    context.font = "italic 30px Arial"
+    context.fillText(certificate_id, 1000, image.height - 60)
 
     // Save image to file
     const modified_image = canvas.toBuffer("image/png")
-    fs.writeFileSync("src/assets/certificate.png", modified_image)
+    const image_path = "src/assets/certificate" + `_${certificate.serial_number.toString()}.png`
+    fs.writeFileSync(image_path, modified_image)
 
     return certificate
 }
@@ -72,7 +86,7 @@ async function createCertificate (student_course_report) {
  * @throws {Error} if student has not completed course
  */
 exports.issueCertificate = async (report_id) => {
-    const student_course_report = await CourseReport.findById( report_id ).populate({
+    const student_course_report = await CourseReport.findById(report_id).populate({
         path: "course",
         select: "title description _id",
         populate: {
@@ -95,13 +109,15 @@ exports.issueCertificate = async (report_id) => {
 
     // Upload file to cloudinary
     const file_url = await uploadToCloudinary({
-        path: "src/assets/certificate.png",
+        path: "src/assets/certificate" + `_${certificate.serial_number.toString()}.png`,
         file_name: `certificate_${certificate._id}`,
         destination_path: `course_${certificate.course._id}/user_${certificate.user._id}`,
     });
 
     // Save file url to database
-    certificate = await certificate.updateOne({ file_url });
+    certificate.certificate_url = file_url,
+    
+    certificate = await certificate.save()
 
     return certificate
 }
