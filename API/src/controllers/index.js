5c04d8208352200dabe1345c85cd583c7e79caed
Richie <molunorichie@gmail.com>
2023-03-20 17:25:23 +0100

    remove index file for controllers

--- API/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-    ...require('./auth.controllers'),
-    ...require('./course.controllers'),
-    ...require('./exercise.controllers'),
-    ...require('./courseReport.controllers'),
-    ...require('./textmaterial.controllers')
-}
