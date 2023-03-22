b58226fd5af630fc20a98de7bae71fcfdcde2804
Richie <molunorichie@gmail.com>
2023-03-22 23:27:14 +0100

    add: isCompletedStatus to exercise

--- API/src/models/course.models.js
+++ API/src/models/course.models.js
@@ -213,7 +213,7 @@ exerciseSchema.virtual('questions', {
     foreignField: 'exercise',
     ref: 'Question'
 })
-
+    
 /**
  * @type {videoSchema}
  * */
@@ -297,7 +297,17 @@ courseSectionSchema.virtual('textmaterials', {
  * @param {courseSectionSchema} courseSection 
  * @returns {courseSectionSchema}
  */
-function combineContents(courseSection) {
+function combineContents(courseSection, studentCourseReport) {
+    if (studentCourseReport) {
+        console.log(studentCourseReport)
+        courseSection.isCompleted = studentCourseReport.completed_course_sections.includes(courseSection._id)
+
+        courseSection.exercises = courseSection.exercises.map(exercise => {
+            exercise.isCompleted = studentCourseReport.completed_exercises.includes(exercise._id)
+            return exercise
+        })
+    }
+
     courseSection.contents = [
         ...courseSection.videos??[],
         ...courseSection.exercises??[],
@@ -306,17 +316,33 @@ function combineContents(courseSection) {
 
     courseSection.contents.sort((a, b) => {
         return a.order - b.order
-    }
-    )
+    })
 
     return courseSection
 }
 
-courseSectionSchema.post('find', async function (courseSections) {
-    for (let courseSection of courseSections) {
-        await combineContents(courseSection)
-    }
-})
+async function getCompletedExercisesInCourseSection(course, studentCourseReport) {
+    const courseSections = await course.populate({
+        path: 'course_sections',
+        populate: {
+            path: 'exercises',
+            model: 'Exercise'
+        }
+    })
+    console.log(courseSections.toObject().course_sections)
+    const completedExercises = courseSections.toObject().course_sections.map(courseSection => {
+        console.log(courseSection)
+        return courseSection.exercises.filter(exercise => {
+            exercise.isCompleted = studentCourseReport.completed_exercises.includes(exercise._id)
+            
+            return exercise
+        })
+    })
+
+    console.log(completedExercises)
+
+    return completedExercises
+}
 
 courseSectionSchema.post('findOne', async function (courseSection) {
     const doc = await combineContents(courseSection)
@@ -424,4 +450,5 @@ module.exports = {
     Question, Exercise,
     CourseReport, TextMaterial,
     ExerciseSubmission,
+    getCompletedExercisesInCourseSection
 };
