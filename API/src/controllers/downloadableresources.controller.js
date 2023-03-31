1e5badd4d620936c4c08c8ce61ba87cc89c0c9d2
Richie <molunorichie@gmail.com>
2023-03-31 22:47:00 +0100

    add: getDownloadableResourceData

--- API/src/controllers/downloadableresources.controller.js
+++ API/src/controllers/downloadableresources.controller.js
@@ -1,7 +1,7 @@
 const { DownloadableResources, User } = require('../models/downloadableresources.model');
 const { User } = require('../models/user.model');
 
-const { BadRequestError } = require('../utils/errors');
+const { BadRequestError, NotFoundError } = require('../utils/errors');
 
 /**
  * Get all downloadable resources
@@ -33,7 +33,37 @@ exports.getDownloadableResources = async (req, res, next) => {
     })
 }
 
+/**
+ * Get downloadable resource data
+ * 
+ * @description This function is used to get downloadable resource data
+ * 
+ * @param {String} id - The resource id
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {NextFunction} next - Express next function
+ * 
+ * @returns {Promise<void>}
+ */
 exports.getDownloadableResourceData = async (req, res, next) => {
+    const { id } = req.params;
+
+    // Check if id is provided
+    if (!id || id == null || id == undefined) {
+        return next(new BadRequestError('Resource id is required'));
+    }
+
+    const downloadable_resource = await DownloadableResources.findById(id);
+    if (!downloadable_resource) {
+        return next(new NotFoundError('Resource not found'));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            downloadable_resource
+        }
+    })
 }
 
 exports.deleteDownloadableResource = async (req, res, next) => {
