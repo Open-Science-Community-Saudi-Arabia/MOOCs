67c4c16cd2488b47332a3abd85c67f7b32b139e0
Richie <molunorichie@gmail.com>
2023-03-31 22:44:21 +0100

    add: getDownloadableResources

---
+++ API/src/controllers/downloadableresources.controller.js
@@ -0,0 +1,46 @@
+const { DownloadableResources, User } = require('../models/downloadableresources.model');
+const { User } = require('../models/user.model');
+
+const { BadRequestError } = require('../utils/errors');
+
+/**
+ * Get all downloadable resources
+ * 
+ * @description This function is used to get all downloadable resources
+ * 
+ * @param {String} id - The course id
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {NextFunction} next - Express next function
+ * 
+ * @returns {Promise<void>}
+ */
+exports.getDownloadableResources = async (req, res, next) => {
+    const { id } = req.params;
+
+    // Check if id is provided
+    if (!id || id == null || id == undefined) {
+        return next(new BadRequestError('Course id is required'));
+    }
+
+    const downloadable_resources = await DownloadableResources.find({ course: id });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            downloadable_resources
+        }
+    })
+}
+
+exports.getDownloadableResourceData = async (req, res, next) => {
+}
+
+exports.deleteDownloadableResource = async (req, res, next) => {
+}
+
+exports.updateDownloadableResource = async (req, res, next) => {
+}
+
+exports.uploadDownloadableResource = async (req, res, next) => {
+}
