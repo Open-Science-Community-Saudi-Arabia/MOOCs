ec010bd71218d6c87b043e2ec76dbfe1931d3343
Richie <molunorichie@gmail.com>
2023-04-10 12:49:18 +0100

    change email verification template direction for arabic

--- API/src/utils/email/templates/index.js
+++ API/src/utils/email/templates/index.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 
 module.exports = {
     email_verification_template: (name, verification_link) => ejs.render(fs.readFileSync(__dirname + '/email_verification_template.ejs', 'utf8'), { name, verification_link }),
-    password_reset_template:(name, reset_code) => ejs.render(fs.readFileSync(__dirname + '/password_reset_template.ejs', 'utf8'), { name, reset_code})
+    password_reset_template: (name, reset_code) => ejs.render(fs.readFileSync(__dirname + '/password_reset_template.ejs', 'utf8'), { name, reset_code })
 }
