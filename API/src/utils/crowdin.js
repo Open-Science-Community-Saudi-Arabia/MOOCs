c33b916f8d842a4b82082fd814a9ff94afd45ab7
Richie <molunorichie@gmail.com>
2023-04-13 03:59:40 +0100

    first translation attempt working

--- API/src/utils/crowdin.js
+++ API/src/utils/crowdin.js
@@ -1,9 +1,11 @@
 const crowdinAPI = require('crowdin-api');
-const { CROWDIN_PROJECT_ID, CROWDIN_API_KEY } = require('./config');
-const CROWDIN_API_ENDPOINT = 'https://api.crowdin.com/api/v2'
+const CROWDIN_API = 'https://api.crowdin.com/api/v2'
+const CROWDIN_PROJECT_ID = '580127'
+const CROWDIN_API_KEY = '7ab2599a42f3aa76170d1c8bb7c2464ba3f16143c77d5fa9abd5e0edf19eeecf5880aa49fc06c431'
+const STORAGE_ID = 1845177783
 const crowdin = new crowdinAPI({
     apiKey: CROWDIN_API_KEY,
-    projectName: 'openinnovationlab',
+    projectName: 'Open Science MOOCs',
 })
 const axios = require('axios');
 // // Example API route
@@ -45,38 +47,97 @@ const axios = require('axios');
 
 //     return JSON.parse(response.data.translations[0].data);
 // }
-async function translateResponse(response) {
+
+const auth = {
+    headers: {
+        authorization: 'Bearer ' + CROWDIN_API_KEY,
+        'Content-Type': 'application/json',
+        'Crowdin-API-FileName': 'MOOCS API',
+    },
+}
+async function translateResponse() {
     try {
-        const text_to_translate = JSON.stringify(response)
+        console.log(CROWDIN_API_KEY)
+        console.log(CROWDIN_PROJECT_ID)
 
-        const translated_response = await axios.post(
-            `${CROWDIN_API_ENDPOINT}/translations`,
-            {
-                projectId: CROWDIN_PROJECT_ID, // Replace with your CROWDIN project ID
-                languageId: 'ar',
-                files: [
-                    {
-                        name: 'course_data.json', // Replace with your file name
-                        content: JSON.stringify(text_to_translate),
-                    },
-                ],
-                autoApproveOption: '1',
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${CROWDIN_API_KEY}`,
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
+        // CREATE STORAGE
+        // const storages = await axios.post('https://api.crowdin.com/api/v2/storages', {
+        //     'Crowdin-API-FileName': 'MOOCSAPI.json',
+        // }, auth)
+        // console.log(storages.data.data[0])
+        // console.log(storages.data)
 
-        console.log(translated_response.data)
-        return translated_response.data
+        // ADD FILE
+        // const res = await axios.post('https://api.crowdin.com/api/v2/projects/580127/files', 
+        // {
+        //     "storageId": 1845177783,
+        //     "name": "course.json",
+        //     "title": "Course data",
+        //     "type": "json",
+        // }, auth)
+
+
+        // EDIT FILE
+        // const res = await axios.patch('https://api.crowdin.com/api/v2/projects/580127/files/3', [
+        //     { "op": "add", "path": "/title", "value": "this is the beginning" }
+        // ], auth)
+
+        // console.log(res.data)
+
+        // ADD STRINGS FOR TRANSLATION
+        // const res = await axios.post('https://api.crowdin.com/api/v2/projects/580127/strings', {
+        //     text: 'This is a test string',
+        //     identifier: 'This is a test string',
+        //     fileId: 3,
+        // }, auth)
+
+        // console.log(res.data)
+
+        // GET MACHINE TRANSLATION
+        // const res = await axios.get('https://api.crowdin.com/api/v2/mts', auth)
+        // console.log(res.data.data[0])
+
+        //  Translate via MT
+        const res = await axios.post('https://api.crowdin.com/api/v2/mts/372727/translations', {
+            "languageRecognitionProvider": "crowdin",
+            "targetLanguageId": "ar",
+            "sourceLanguageId": "en",
+            "strings": [
+                "Hi my name is Richie",
+                "I am a software engineer",
+                "I love to code",
+                "I love to code in JavaScript",
+            ]
+        }, auth)
+
+        console.log(res.data)
+
+
+        // const res = await axios.post(
+        //     'https://api.crowdin.com/api/v2/projects/580127/files',
+        //     {
+        //         "name": "MOOCS API",
+        //         "title": "MOOCS API",
+        //         "exportPattern": "/localization/%locale%/%file_name%",
+        //         "priority": "normal"
+        //     },
+        //     auth
+        // ).then().catch()
+
+        // console.log({
+        //     ...res.data
+        // })
+
+        // console.log(res.data.data[0])
     } catch (error) {
-        console.log(error)
+        console.log(error.response.data)
+        console.log(error.response.data.errors)
+        console.log(error.response.data.errors[0].error)
         return error
     }
 }
+
+translateResponse()
 
 module.exports = {
     translateResponse
