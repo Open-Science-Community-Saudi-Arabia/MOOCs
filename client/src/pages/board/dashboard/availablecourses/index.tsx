a0553c9410b274c78443be38b64e37e9a58cbc17
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2023-08-13 17:22:05 +0100

    fixed no course available

--- client/src/pages/board/dashboard/availablecourses/index.tsx
+++ client/src/pages/board/dashboard/availablecourses/index.tsx
@@ -30,52 +30,59 @@ const AvailableCourses = ({ courses }: any) => {
       });
     }
   };
+
   return (
     <div className="availablecourses ">
-      <h1 className="availablecourses__heading aligned">
-        <Trans>Available Courses</Trans>
-      </h1>
-      <div className="availablecourses__courses">
-        {courses?.map((content: Courses) => {
-          return (
-            <button
-              onClick={() => enrollUserHandler(content._id)}
-              aria-label={content.title}
-              key={content._id}
-              style={{ width: "300px", height: "300px" }}
-              className="availablecourses__courses-content"
-            >
-              <div className="availablecourses__courses-content__img-container">
-                {" "}
-                <div className={"img-container-overlay"}>
-                  {isLoading && selectedId === content._id ? (
-                    <Spinner width="50px" height="50px" color="#0a0a0a" />
-                  ) : (
-                    <BsFillPlayCircleFill />
-                  )}
-                </div>
-                <img
-                  className="availablecourses__courses-content__img-container-img"
-                  src={content.preview_image}
-                  alt={content.title}
-                />
-              </div>
+      {courses.length ? (
+        <>
+          <h1 className="availablecourses__heading aligned">
+            <Trans>Available Courses</Trans>
+          </h1>
+          <div className="availablecourses__courses">
+            {courses?.map((content: Courses) => {
+              return (
+                <button
+                  onClick={() => enrollUserHandler(content._id)}
+                  aria-label={content.title}
+                  key={content._id}
+                  style={{ width: "300px", height: "300px" }}
+                  className="availablecourses__courses-content"
+                >
+                  <div className="availablecourses__courses-content__img-container">
+                    {" "}
+                    <div className={"img-container-overlay"}>
+                      {isLoading && selectedId === content._id ? (
+                        <Spinner width="50px" height="50px" color="#0a0a0a" />
+                      ) : (
+                        <BsFillPlayCircleFill />
+                      )}
+                    </div>
+                    <img
+                      className="availablecourses__courses-content__img-container-img"
+                      src={content.preview_image}
+                      alt={content.title}
+                    />
+                  </div>
 
-              <div className="availablecourses__courses-content__bottom aligned">
-                <p className="availablecourses__courses-content__bottom-text">
-                  {locale === "en" ? content.title : content.title_tr}
-                </p>
-                <p className="availablecourses__courses-content__bottom-author">
-                  {" "}
-                  {locale === "en"
-                    ? content.description
-                    : content.description_tr}
-                </p>
-              </div>
-            </button>
-          );
-        })}
-      </div>
+                  <div className="availablecourses__courses-content__bottom aligned">
+                    <p className="availablecourses__courses-content__bottom-text">
+                      {locale === "en" ? content.title : content.title_tr}
+                    </p>
+                    <p className="availablecourses__courses-content__bottom-author">
+                      {" "}
+                      {locale === "en"
+                        ? content.description
+                        : content.description_tr}
+                    </p>
+                  </div>
+                </button>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <p className="no-content">No Courses Available</p>
+      )}
     </div>
   );
 };
