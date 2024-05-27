4f97758c6e4ee13bc7627411a2821e0b110ec38b
sandygudie <goodnewssandy@gmail.com>
2024-05-27 14:54:31 +0100

    Chore updated github action

--- client/src/components/Layouts/dashboard.tsx
+++ client/src/components/Layouts/dashboard.tsx
@@ -2,21 +2,21 @@ import Header from "../DashboardHeader";
 import "./style.scss";
 import { Outlet } from "react-router-dom";
 
- /**
-   * @category Client
-   * @subcategory Component
-   * @module Dashboard Layout
-   * @description The layout component wraps the protected pages using react-router-outlet.
-   * @component
-   * @example
+/**
+ * @category Client
+ * @subcategory Component
+ * @module Dashboard Layout
+ * @description The layout component wraps the protected pages using react-router-outlet.
+ * @component
+ * @example
  *  <DashboardLayout />
-   */
+ */
 
 export default function DashboardLayout() {
   return (
     <section className="dashboard_layout">
       <Header />
-      <div className="bg-[#f5f5f7] px-6 md:px-8 md:px-12 mx-auto pt-28 pb-12">
+      <div className="px-6 md:px-8 md:px-12 mx-auto pt-28 pb-12">
         <Outlet />
       </div>
     </section>
